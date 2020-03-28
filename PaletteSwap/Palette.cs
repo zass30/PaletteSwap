a8e07118880c971f2682d667cbe52285539cf107
zass30 <jbeasley@gmail.com>
2020-03-27 21:54:12 -0700

    PaletteConfig used in character generation

--- PaletteSwap/Palette.cs
+++ PaletteSwap/Palette.cs
@@ -25,6 +25,7 @@ namespace PaletteSwap
         {
             byte[] colordefaults = PaletteHelper.StringToByteStream(defaults);
             byte[] col_byte = new byte[2];
+            this.defaultColorOffsets = new List<ColorOffset>();
             for (int i = 0; i < colordefaults.Length; i = i + 2)
             {
                 col_byte[0] = colordefaults[i];
@@ -35,6 +36,59 @@ namespace PaletteSwap
                 cp.position = offset + i;
                 this.defaultColorOffsets.Add(cp);
             }
+        }
+
+        public static PaletteConfig GenerateDictatorConfig()
+        {
+            int ROWLEN = 32;
+            int MEMLEN = 160;
+            Dictionary<string, List<int>> dictatorSpriteOffsets = new Dictionary<string, List<int>>
+        {
+            { "pads5", new List<int>() { 0, ROWLEN * 3 + 0, ROWLEN * 4 + 0 } },
+            { "costume5", new List<int>() { 2, ROWLEN * 3 + 2, ROWLEN * 4 + 2 } },
+            { "costume4", new List<int>() { 4, ROWLEN * 3 + 4, ROWLEN * 4 + 4 } },
+            { "costume3", new List<int>() { 6, ROWLEN * 3 + 6, ROWLEN * 4 + 6 } },
+            { "costume2", new List<int>() { 8, ROWLEN * 3 + 8, ROWLEN * 4 + 8 } },
+            { "costume1", new List<int>() { 10, ROWLEN * 3 + 10, ROWLEN * 4 + 10 } },
+            { "pads4", new List<int>() { 12 } },
+            { "stripe", new List<int>() { 14, ROWLEN * 3 + 14 } },
+            { "pads1", new List<int>() { 16, ROWLEN * 1 + 16, ROWLEN * 3 + 16 } },
+            { "pads2", new List<int>() { 18, ROWLEN * 1 + 18, ROWLEN * 3 + 18 } },
+            { "pads3", new List<int>() { 20, ROWLEN * 1 + 20, ROWLEN * 3 + 20 } },
+            { "skin1", new List<int>() { 22, ROWLEN * 1 + 22, ROWLEN * 3 + 22, ROWLEN * 4 + 22 } },
+            { "skin2", new List<int>() { 24, ROWLEN * 1 + 24, ROWLEN * 3 + 24, ROWLEN * 4 + 24 } },
+            { "skin3", new List<int>() { 26, ROWLEN * 1 + 26, ROWLEN * 3 + 26, ROWLEN * 4 + 26 } },
+            { "skin4", new List<int>() { 28, ROWLEN * 1 + 28, ROWLEN * 3 + 28, ROWLEN * 4 + 28 } },
+
+            { "crusherpads5", new List<int>() { ROWLEN * 2 + 0 } },
+            { "crushercostume4", new List<int>() { ROWLEN * 2 + 4 } },
+            { "crushercostume3", new List<int>() { ROWLEN * 2 + 6 } },
+            { "crushercostume2", new List<int>() { ROWLEN * 2 + 8 } },
+            { "crushercostume1", new List<int>() { ROWLEN * 2 + 10 } },
+            { "crusherpads4", new List<int>() { ROWLEN * 2 + 12 } },
+            { "crusherflame1", new List<int>() { ROWLEN * 2 + 14 } },
+            { "crusherpads1", new List<int>() { ROWLEN * 2 + 16 } },
+            { "crusherpads2", new List<int>() { ROWLEN * 2 + 18 } },
+            { "crusherpads3", new List<int>() { ROWLEN * 2 + 20 } },
+            { "crusherflame2", new List<int>() { ROWLEN * 2 + 22 } },
+            { "crusherhands1", new List<int>() { ROWLEN * 2 + 24 } },
+            { "crusherhands2", new List<int>() { ROWLEN * 2 + 26 } },
+            { "psychoglow", new List<int>() { ROWLEN * 3 + 12 } },
+            { "psychopunch1", new List<int>() { ROWLEN * 4 + 12 } },
+            { "psychopunch2", new List<int>() { ROWLEN * 4 + 14 } },
+            { "psychopunch3", new List<int>() { ROWLEN * 4 + 16 } },
+            { "psychopunch4", new List<int>() { ROWLEN * 4 + 18 } },
+            { "psychopunch5", new List<int>() { ROWLEN * 4 + 20 } },
+        };
+
+            PaletteConfig pc = new PaletteConfig();
+            pc.labelOffsets = dictatorSpriteOffsets;
+            pc.unusedOffsets = new List<int>() { 66, 67, 92, 93 };
+            string defaults = "0007 2302 3403 5605 6706 7807 8A08 9B09";
+            int defaultsOffset = 32;
+            pc.createColorOffsets(defaults, defaultsOffset);
+            pc.streamLength = MEMLEN;
+            return pc;
         }
     }
 
@@ -95,10 +149,11 @@ namespace PaletteSwap
             var c = new Character();
             if (characater == CHARACTERS.Dictator)
             {
-                Palette s = new Palette();
+                PaletteConfig pc = PaletteConfig.GenerateDictatorConfig();
+                Palette s = Palette.PaletteFromConfig(pc);
                 Palette p = new Palette();
                 c.sprite = s;
-                s.setAllOffSets(dictatorSpriteOffsets);
+//                s.setAllOffSets(dictatorSpriteOffsets);
                 byte[] b = new byte[0];                
                 switch (button)
                 {
@@ -134,6 +189,8 @@ namespace PaletteSwap
                         break;
                 }
                 s.loadStream(b);
+
+                /*
                 s.streamLength = b.Length;
                 byte[] colordefaults = PaletteHelper.StringToByteStream(defaults);
                 byte[] col_byte = new byte[2];
@@ -148,6 +205,7 @@ namespace PaletteSwap
                     s.defaultColorOffsets.Add(cp);
                 }
                 s.unusedOffsets = new List<int>() { 66, 67, 92, 93 };
+                */
                 c.portrait = p;                
             }
             return c;
@@ -159,7 +217,6 @@ namespace PaletteSwap
         public Color c;
         public int position;
     }
-
 
     public class Palette
     {
--- PaletteSwapTest/PaletteTest.cs
+++ PaletteSwapTest/PaletteTest.cs
@@ -9,58 +9,7 @@ namespace PaletteSwapTest
     [TestClass]
     public class PaletteTest
     {
-        private PaletteConfig GenerateDictatorConfig()
-        {
-            int ROWLEN = 32;
-            int MEMLEN = 160;
-            Dictionary<string, List<int>> dictatorSpriteOffsets = new Dictionary<string, List<int>>
-        {
-            { "pads5", new List<int>() { 0, ROWLEN * 3 + 0, ROWLEN * 4 + 0 } },
-            { "costume5", new List<int>() { 2, ROWLEN * 3 + 2, ROWLEN * 4 + 2 } },
-            { "costume4", new List<int>() { 4, ROWLEN * 3 + 4, ROWLEN * 4 + 4 } },
-            { "costume3", new List<int>() { 6, ROWLEN * 3 + 6, ROWLEN * 4 + 6 } },
-            { "costume2", new List<int>() { 8, ROWLEN * 3 + 8, ROWLEN * 4 + 8 } },
-            { "costume1", new List<int>() { 10, ROWLEN * 3 + 10, ROWLEN * 4 + 10 } },
-            { "pads4", new List<int>() { 12 } },
-            { "stripe", new List<int>() { 14, ROWLEN * 3 + 14 } },
-            { "pads1", new List<int>() { 16, ROWLEN * 1 + 16, ROWLEN * 3 + 16 } },
-            { "pads2", new List<int>() { 18, ROWLEN * 1 + 18, ROWLEN * 3 + 18 } },
-            { "pads3", new List<int>() { 20, ROWLEN * 1 + 20, ROWLEN * 3 + 20 } },
-            { "skin1", new List<int>() { 22, ROWLEN * 1 + 22, ROWLEN * 3 + 22, ROWLEN * 4 + 22 } },
-            { "skin2", new List<int>() { 24, ROWLEN * 1 + 24, ROWLEN * 3 + 24, ROWLEN * 4 + 24 } },
-            { "skin3", new List<int>() { 26, ROWLEN * 1 + 26, ROWLEN * 3 + 26, ROWLEN * 4 + 26 } },
-            { "skin4", new List<int>() { 28, ROWLEN * 1 + 28, ROWLEN * 3 + 28, ROWLEN * 4 + 28 } },
 
-            { "crusherpads5", new List<int>() { ROWLEN * 2 + 0 } },
-            { "crushercostume4", new List<int>() { ROWLEN * 2 + 4 } },
-            { "crushercostume3", new List<int>() { ROWLEN * 2 + 6 } },
-            { "crushercostume2", new List<int>() { ROWLEN * 2 + 8 } },
-            { "crushercostume1", new List<int>() { ROWLEN * 2 + 10 } },
-            { "crusherpads4", new List<int>() { ROWLEN * 2 + 12 } },
-            { "crusherflame1", new List<int>() { ROWLEN * 2 + 14 } },
-            { "crusherpads1", new List<int>() { ROWLEN * 2 + 16 } },
-            { "crusherpads2", new List<int>() { ROWLEN * 2 + 18 } },
-            { "crusherpads3", new List<int>() { ROWLEN * 2 + 20 } },
-            { "crusherflame2", new List<int>() { ROWLEN * 2 + 22 } },
-            { "crusherhands1", new List<int>() { ROWLEN * 2 + 24 } },
-            { "crusherhands2", new List<int>() { ROWLEN * 2 + 26 } },
-            { "psychoglow", new List<int>() { ROWLEN * 3 + 12 } },
-            { "psychopunch1", new List<int>() { ROWLEN * 4 + 12 } },
-            { "psychopunch2", new List<int>() { ROWLEN * 4 + 14 } },
-            { "psychopunch3", new List<int>() { ROWLEN * 4 + 16 } },
-            { "psychopunch4", new List<int>() { ROWLEN * 4 + 18 } },
-            { "psychopunch5", new List<int>() { ROWLEN * 4 + 20 } },
-        };
-
-            PaletteConfig pc = new PaletteConfig();
-            pc.labelOffsets = dictatorSpriteOffsets;
-            pc.unusedOffsets = new List<int>() { 66, 67, 92, 93 };
-            string defaults = "0007 2302 3403 5605 6706 7807 8A08 9B09";
-            int defaultsOffset = 32;
-            pc.createColorOffsets(defaults, defaultsOffset);
-            pc.streamLength = MEMLEN;
-            return pc;
-        }
 
         [TestMethod]
         public void PaletteNewTest()
