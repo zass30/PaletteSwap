a00510ca17787f47303a3757151d7231350af8d0
zass30 <jbeasley@gmail.com>
2020-04-03 15:33:43 -0700

    preview color set window works

--- PaletteSwap/ColorSetForm.cs
+++ PaletteSwap/ColorSetForm.cs
@@ -46,6 +46,29 @@ namespace PaletteSwap
                 if (mainform.characterSet.characterColors[9].sprite != null)
                     _09psychopunchBox.Image = mainform.characterSet.characterColors[9].sprite.GetBitmap("psychopunch");
             }
+            else if (mainform.currentCharacterType == Character.CHARACTERS.Claw)
+            {
+                if (mainform.characterSet.characterColors[0].sprite != null)
+                    _00psychopunchBox.Image = mainform.characterSet.characterColors[0].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[1].sprite != null)
+                    _01psychopunchBox.Image = mainform.characterSet.characterColors[1].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[2].sprite != null)
+                    _02psychopunchBox.Image = mainform.characterSet.characterColors[2].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[3].sprite != null)
+                    _03psychopunchBox.Image = mainform.characterSet.characterColors[3].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[4].sprite != null)
+                    _04psychopunchBox.Image = mainform.characterSet.characterColors[4].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[5].sprite != null)
+                    _05psychopunchBox.Image = mainform.characterSet.characterColors[5].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[6].sprite != null)
+                    _06psychopunchBox.Image = mainform.characterSet.characterColors[6].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[7].sprite != null)
+                    _07psychopunchBox.Image = mainform.characterSet.characterColors[7].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[8].sprite != null)
+                    _08psychopunchBox.Image = mainform.characterSet.characterColors[8].sprite.GetBitmap("neutral");
+                if (mainform.characterSet.characterColors[9].sprite != null)
+                    _09psychopunchBox.Image = mainform.characterSet.characterColors[9].sprite.GetBitmap("neutral");
+            }
         }
     }
 }
