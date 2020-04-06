a7163e70ecc0aed944443ac262ab9c36ec341225
zass30 <jbeasley@gmail.com>
2020-04-06 11:08:31 -0700

    removed commented out code

--- PaletteSwap/ZoomForm.cs
+++ PaletteSwap/ZoomForm.cs
@@ -28,10 +28,7 @@ namespace PaletteSwap
         int scale; // how much is image zoomed
         double factor = 1.3; // how much buffer around window and image
         Bitmap magnified_sprite;
-        Bitmap magnified_losstop;
-        Bitmap magnified_lossbottom; // special case for loss portrait which is made of two bitmaps
         img_type zoomed_img;
-        PaletteImage currentImage;
 
         public ZoomForm(MainForm parentform)
         {
@@ -46,74 +43,11 @@ namespace PaletteSwap
         {
             zoomBox.BackgroundImage = mainform.currentlySelectedZoomPaletteImage.RemappedScaledImage();
             return;
-            ColorMap[] remapTable = new ColorMap[0];
-            switch (zoomed_img)
-            {
-                case img_type.neutral:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.sprite.GetImage("neutral").RemappedScaledImage();
-                    break;
-                case img_type.psychopunch:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.sprite.GetImage("psychopunch").RemappedScaledImage();
-                    break;
-                case img_type.psychoprep:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.sprite.GetImage("psychoprep").RemappedScaledImage();
-                    break;
-                case img_type.crushertop:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.sprite.GetImage("crushertop").RemappedScaledImage();
-                    break;
-                case img_type.crusherside:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.sprite.GetImage("crusherbottom").RemappedScaledImage();
-                    break;
-                case img_type.victoryportrait:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.portrait.GetImage("victory").RemappedScaledImage();
-                    break;
-                case img_type.lossportrait:
-                    zoomBox.BackgroundImage = mainform.currentCharacter.portrait.GetImage("losstop").RemappedScaledImage();
-                    break;
-            }
-            
-/*            switch (zoomed_img)
-            {
-                case img_type.neutral:
-                    remapTable = mainform.currentSprite.StandingSpriteColorsRemapTable();
-                    break;
-                case img_type.psychopunch:
-                    remapTable = mainform.currentSprite.PsychoPunchColorsRemapTable();
-                    break;
-                case img_type.psychoprep:
-                     remapTable = mainform.currentSprite.PsychoPrepColorsRemapTable();
-                    break;
-                case img_type.crushertop:
-                     remapTable = mainform.currentSprite.CrusherColorsRemapTable();
-                    break;
-                case img_type.crusherside:
-                    remapTable = mainform.currentSprite.CrusherColorsRemapTable();
-                    break;
-                case img_type.victoryportrait:
-                    remapTable = mainform.currentPortrait.VictoryColorsRemapTable();
-                    break;
-                case img_type.lossportrait:
-                    if (magnified_losstop == null)
-                    {
-                        magnified_losstop = mainform.magnify_sprite(Properties.Resources.dicportraitlosstop5, scale);
-                    }
-                    if (magnified_lossbottom == null)
-                    {
-                        magnified_lossbottom = mainform.magnify_sprite(Properties.Resources.dicportraitlossbottom5, scale);
-
-                    }
-                    mainform.imagepaint(e, magnified_losstop, mainform.currentPortrait.LossTopColorsRemapTable());
-                    mainform.imagepaint(e, magnified_lossbottom, mainform.currentPortrait.LossBottomColorsRemapTable());
-                    return;
-            }
-            mainform.imagepaint(e, magnified_sprite, remapTable);
-            */
         }
 
         public void refreshZoomBox()
         {
             zoomBox.BackgroundImage = mainform.currentlySelectedZoomPaletteImage.RemappedScaledImage();
-            //zoomBox.Refresh();
         }
 
         public void displayZoomImage(Bitmap b, img_type i)
@@ -123,8 +57,6 @@ namespace PaletteSwap
             this.Height = (int) (b.Height * factor * scale);
             this.Width = (int)(b.Width * factor * scale);
             zoomBox.BackgroundImage = mainform.currentlySelectedZoomPaletteImage.RemappedScaledImage();
-//            magnified_sprite = mainform.magnify_sprite(b, scale);
-//            zoomBox.BackgroundImage = magnified_sprite;
             zoomed_img = i;
         }
     }
